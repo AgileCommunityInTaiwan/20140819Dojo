6482fa08b982eebb87530246ae5f3483f1db671d
AgileCommunityTaiwan <AgileComm.tw@gmail.com>
2014-08-15 18:08:00 +0800

    auto commit

---
+++ Dojo/Class1.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        internal static int Foo()
+        {
+            return 1;
+        }
+    }
+}
---
+++ Dojo/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(Class1.Foo(), Is.EqualTo(1));
+        }
+    }
+}
